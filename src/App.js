8f89259fec98defab4a5bf625f307b1b3cd0df30
MouhannadAlKhodor <moesidaya@gmail.com>
2021-03-26 00:47:37 +0200

    last changes

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import Api from "./components/Search";
+
+
+
+
+
+import "./App.css";
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Mouhannad + Zaynab"
+    };
+  }
+
+  handleInputChange = value => {
+    this.setState({ name: value });
+  };
+
+  render() {
+    return (
+      <>
+        {/* <SayHi />
+        <SayHello color="black" name={this.state.name} />
+         */}
+         
+         
+          <Api />
+          
+       </>  
+      
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Currentweather.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+function Currentweather() {
+  return (
+    <div>
+        
+          
+          
+        
+    </div>
+  )
+}
+
+export default Currentweather
